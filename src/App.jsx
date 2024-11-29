6b014393fc08eb23baa2f3f26bbf6205fc5446f6
Rakshitasinghh <rakshita.singhhh@gmail.com>
2024-11-29 22:30:00 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+import Navigation from "./component/navigation";
+const App = () => {
+    return (
+        <div>
+           <Navigation/>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/component/navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () =>{
+    return(
+       <nav>
+       <div className="logo">
+           <img src="/images/brand_logo.png" alt="Logo" />
+       </div>
+       <ul>
+           <li herf ="#">Menu</li>
+           <li herf ="#">Location</li>
+           <li herf ="#">About</li>
+           <li herf ="#">Contact</li>
+       </ul>
+       <button>login</button>
+       
+   </nav>
+    );
+   };
+    export default Navigation ;
